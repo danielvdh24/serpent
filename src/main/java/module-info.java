cba0f987e2bcc45045eb5f1a727395d224eee252
gusmojsma <gusmojsma@gu.student.se>
2022-12-01 15:21:01 +0100

    Reset branch

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.serpentmain {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.serpentmain to javafx.fxml;
-    exports com.example.serpentmain;
-}

68b28ce6c51eac3b08d972648d6697b3dfe1ede2
Erik Nisbet <eriknis@net.chalmers.se>
2022-12-21 00:52:38 +0100

    dropshadow behind buttons test

--- src/main/java/com/projectserpent/frontend/LeaderboardController.java
+++ src/main/java/com/projectserpent/frontend/LeaderboardController.java
@@ -11,8 +11,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 
 import java.io.IOException;
 

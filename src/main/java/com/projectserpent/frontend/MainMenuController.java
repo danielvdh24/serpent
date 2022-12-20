c90e43f45a1afd2bb811ba5225ad58e33007ce35
danielvdh24 <vandenheuveldaniel2004@gmail.com>
2022-12-20 04:18:26 +0100

    Additional GUI changes in the Main Menu Scene Done by Erik and Daniel

--- src/main/java/com/projectserpent/frontend/MainMenuController.java
+++ src/main/java/com/projectserpent/frontend/MainMenuController.java
@@ -1,20 +1,50 @@
 package com.projectserpent.frontend;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 
 public class MainMenuController extends ParentController {
 
+    public ImageView startButtonHovered;
+    public ImageView startButtonNormal;
+    public ImageView leaderboardHoveredImage;
+    public ImageView leaderboardNormal;
+
+    public void initialize() {
+        startButtonHovered.setVisible(false);
+        leaderboardHoveredImage.setVisible(false);
+    }
+
     @FXML
-    public void switchToLeaderboard(MouseEvent event) throws IOException {
+    public void switchToLeaderboard(MouseEvent event) throws IOException, InterruptedException {
         sceneSwitch(event, "leaderboard.fxml");
     }
 
     @FXML
-    public void startGame(MouseEvent event) throws IOException {
+    public void startGame(MouseEvent event) throws IOException, InterruptedException {
         sceneSwitch(event, "game-screen.fxml");
     }
 
-}
+    public void hoverStartButton(MouseEvent event) {
+        startButtonHovered.setVisible(true);
+        startButtonNormal.setVisible(false);
+    }
 
+    public void stopHoverStartButton(MouseEvent event) {
+        startButtonHovered.setVisible(false);
+        startButtonNormal.setVisible(true);
+    }
+
+    public void hoverLeaderboardButton(MouseEvent event) {
+        leaderboardHoveredImage.setVisible(true);
+        leaderboardNormal.setVisible(false);
+    }
+
+    public void stopHoverLeaderboardButton(MouseEvent event) {
+        leaderboardHoveredImage.setVisible(false);
+        leaderboardNormal.setVisible(true);
+    }
+
+}
